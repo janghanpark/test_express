a2fa9df6119ea1fe88bb789926016702c550d363
Janghan Park <janghan.park@posnet.com.au>
2024-02-16 10:04:26 +1100

    asdfasdF

--- app.js
+++ app.js
@@ -25,14 +25,22 @@ app.post("/login", async (req, res) => {
   const { code } = req.body; // Authorization grant code received from Apple
 
   try {
-    const { data } = await axios.post("https://appleid.apple.com/auth/token", {
-      grant_type: "authorization_code",
-      code,
-      client_id: "poble.membership.test",
-      client_secret:
-        "eyJhbGciOiJFUzI1NiIsImtpZCI6Ik5TODhSTkJEN1UifQ.eyJhdWQiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiaXNzIjoiOE4yM1AzNDZGRiIsImlhdCI6MTcwNzk3MTY2MywiZXhwIjoxNzIzNTIzNjY0LCJzdWIiOiJwb2JsZS5tZW1iZXJzaGlwLnRlc3QifQ.XvaszVaex19_tG3P3UYQ4n8aZgGCUhIoPakc5TOVFdaGKkXZAoVCgmbjFlqFOSXI_EwEHQD0STdVhxdgz4t-8g",
-      redirect_uri: "https://test-express-zaer.onrender.com",
-    });
+    const { data } = await axios.post(
+      "https://appleid.apple.com/auth/token",
+      {
+        grant_type: "authorization_code",
+        code,
+        client_id: "poble.membership.test",
+        client_secret:
+          "eyJhbGciOiJFUzI1NiIsImtpZCI6Ik5TODhSTkJEN1UifQ.eyJhdWQiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiaXNzIjoiOE4yM1AzNDZGRiIsImlhdCI6MTcwNzk3MTY2MywiZXhwIjoxNzIzNTIzNjY0LCJzdWIiOiJwb2JsZS5tZW1iZXJzaGlwLnRlc3QifQ.XvaszVaex19_tG3P3UYQ4n8aZgGCUhIoPakc5TOVFdaGKkXZAoVCgmbjFlqFOSXI_EwEHQD0STdVhxdgz4t-8g",
+        redirect_uri: "https://test-express-zaer.onrender.com",
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
 
     console.log(data);
   } catch (error) {
