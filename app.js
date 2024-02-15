8559c3a951fcfc8e64521db0abc9685e5af766c1
Janghan Park <janghan.park@posnet.com.au>
2024-02-16 09:26:35 +1100

    ksdfsalkjdnflajksdf

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post("/login", async (req, res) => {
     code,
     client_id: 'poble.membership.test',
     client_secret: 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ik5TODhSTkJEN1UifQ.eyJhdWQiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiaXNzIjoiOE4yM1AzNDZGRiIsImlhdCI6MTcwNzk3MTY2MywiZXhwIjoxNzIzNTIzNjY0LCJzdWIiOiJwb2JsZS5tZW1iZXJzaGlwLnRlc3QifQ.XvaszVaex19_tG3P3UYQ4n8aZgGCUhIoPakc5TOVFdaGKkXZAoVCgmbjFlqFOSXI_EwEHQD0STdVhxdgz4t-8g',
-    redirect_uri: 'http://localhost:3000/signin',
+    redirect_uri: 'https://test-express-zaer.onrender.com',
   });
 
   console.log(data);
