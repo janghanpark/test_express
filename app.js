6b22aadd0ae8f7e2f999f331f34582048912b3e5
Janghan Park <janghan.park@posnet.com.au>
2024-08-09 10:40:13 +1000

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Import required modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const axios = require("axios");
 
 // Create an instance of Express
@@ -9,42 +11,17 @@ const app = express();
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors());
+app.use(cookieParser());
+
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.json());
 
 // Define a route
 app.get("/", (req, res) => {
-  console.log("hello");
+  console.log(req.cookies);
+
   res.send("Hello, World!");
-});
-
-app.post("/login", async (req, res) => {
-  console.log("Login request");
-  console.log(req.body);
-
-  const { code } = req.body; // Authorization grant code received from Apple
-
-  try {
-    const { data } = await axios.post(
-      "https://appleid.apple.com/auth/token",
-      {
-        grant_type: "authorization_code",
-        code,
-        client_id: "poble.membership.test",
-        client_secret:
-          "eyJhbGciOiJFUzI1NiIsImtpZCI6Ik5TODhSTkJEN1UifQ.eyJhdWQiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiaXNzIjoiOE4yM1AzNDZGRiIsImlhdCI6MTcwNzk3MTY2MywiZXhwIjoxNzIzNTIzNjY0LCJzdWIiOiJwb2JsZS5tZW1iZXJzaGlwLnRlc3QifQ.XvaszVaex19_tG3P3UYQ4n8aZgGCUhIoPakc5TOVFdaGKkXZAoVCgmbjFlqFOSXI_EwEHQD0STdVhxdgz4t-8g",
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
 });
 
 // Start the server
